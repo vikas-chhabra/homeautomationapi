f978f77f727ce314da07678fc184095bdf9d520d
Vikas Chhabra <vikaschhabra.nugen@gmail.com>
2019-11-06 22:23:29 +0530

    removed index

--- app.js
+++ app.js
@@ -41,4 +41,10 @@ app.use((error, req, res, next) => {
     })
 })
 
+const port = process.env.PORT || 8080;
+
+app.listen(port, _=>{
+    console.log("Server running on port, ",port)
+})
+
 module.exports = app;
--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-const port = process.env.PORT || 8080;
-
-app.listen(port, _=>{
-    console.log("Server running on port, ",port)
-})
-
-module.exports = app;
